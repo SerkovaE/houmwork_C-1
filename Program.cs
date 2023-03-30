16ecebf9540eb074eff61fe743e8abc83420ce2b
SerkovaE <128076608+SerkovaE@users.noreply.github.com>
2023-03-30 21:28:47 +0300

    z2
    задача 2

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int a;
+int b;
+Console.WriteLine("Введите два числа.");
+a = Convert.ToInt32(Console.ReadLine());
+b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    Console.Write("max=");
+    Console.Write(a);
+}
+else 
+{
+    Console.Write("max=");
+    Console.Write(b);
+}
